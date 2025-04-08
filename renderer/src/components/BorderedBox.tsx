eb81f7534898464d6d98c20db0883aa7ed004f91
Julian Gruber <julian@juliangruber.com>
2025-04-08 13:40:25 +0200

    fix remove weird border

--- renderer/src/components/BorderedBox.tsx
+++ renderer/src/components/BorderedBox.tsx
@@ -13,7 +13,7 @@ const BorderedBox = ({
 }: BorderedBoxProps) => {
   const className = classNames(
     {
-      'first:rounded-t-md last:rounded-b-md border-t-0 first:border-t': isGrouped,
+      'first:rounded-t-md last:rounded-b-md': isGrouped,
       'rounded-md': !isGrouped
     },
     'bg-[#3A3A3A59] bg-opacity-35 backdrop-blur-3xl',
