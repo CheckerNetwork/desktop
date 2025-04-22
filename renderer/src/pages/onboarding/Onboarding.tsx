e7341339c990a1f4fe8535904f66d5ce8b115651
Julian Gruber <julian@juliangruber.com>
2025-04-22 14:11:14 +0200

    fix loading error

--- renderer/src/pages/onboarding/Onboarding.tsx
+++ renderer/src/pages/onboarding/Onboarding.tsx
@@ -7,13 +7,24 @@ import { ROUTES } from 'src/lib/routes'
 import CheckerLogo from 'src/assets/img/checker-logo.svg?react'
 import OnboardingIllustration from 'src/assets/img/Onboarding Illustration.svg?react'
 
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
+
 const OnboardingPage = (): JSX.Element => {
   const navigate = useNavigate()
   const [isOnboardingCompleted, setIsOnboardingCompleted] = useState<boolean|null>()
 
   useEffect(() => {
     (async () => {
-      setIsOnboardingCompleted(await getOnboardingCompleted())
+      let isOnboardingCompleted
+      while (true) {
+        try {
+          isOnboardingCompleted = await getOnboardingCompleted()
+          break
+        } catch (err) {
+          await sleep(100)
+        }
+      }
+      setIsOnboardingCompleted(isOnboardingCompleted)
     })()
   }, [navigate])
 
